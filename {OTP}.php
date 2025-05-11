fc1a26a73cf0dfd9662be4b6f5caca60d6442dde
Rizky598 <hakimrizky19@gmail.com>
2025-05-12 03:40:35 +0700

    Update {OTP}.php

--- {OTP}.php
+++ {OTP}.php
@@ -1,5 +1,10 @@
 <?php
 
+# Free Recode
+# Kalo lu baik hati, Jangan lupa kasih credit gw yak
+# Updated 03 April 2025
+
+#WARNA
 efunction color($color = "default" , $text  = null)
     {
         $arrayColor = array(
